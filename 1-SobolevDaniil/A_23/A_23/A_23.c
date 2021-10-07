4be933ba3ea4dd3bccb1882a8fcd2d12e63a34e7
Parampaika <92088506+Parampaika@users.noreply.github.com>
2021-10-07 15:09:53 +0300

    Create A_23.c

---
+++ 1-SobolevDaniil/A_23/A_23/A_23.c
@@ -0,0 +1,153 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <locale.h>
+
+
+typedef struct List {
+	char* word;
+	int len;
+	struct List* next;
+}List_t;
+
+void addInfo(List_t* node, char* info) {
+	if (!node) {
+		printf("Error the pointer was NULL\n");
+		exit(1);
+	}
+	node->word = (char*)malloc(strlen(info) + 1);
+	node->len = strlen(info);
+	if (!node->word) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	strcpy(node->word, info);
+	
+}
+
+List_t* insertBeforeNode(List_t** node, List_t** nodeInList, List_t* head) {
+	if (!node || !nodeInList || !head) {
+		printf("Error the pointer was NULL\n");
+		exit(1);
+	}
+	List_t* tmp;
+	tmp = head;
+	if (*nodeInList == head) {
+		(*node)->next = head;
+		head = *(node);
+		return head;
+	}
+	else {
+		while (tmp->next != *(nodeInList)) {
+			tmp = tmp->next;
+		}
+		(*node)->next = tmp->next;
+		tmp->next = *node;
+	}
+	return head;
+}
+List_t* Sort_List(List_t* root, List_t* node) {
+	if (!node || !root) {
+		printf("Error the pointer was NULL\n");
+		exit(1);
+	}
+	List_t* head;
+	head = root;
+
+	while (1) {
+		if (node->len < root->len) {
+			head = insertBeforeNode(&node, &root, head);
+			return head;
+		}
+		else {
+			if (node->len == root->len) {
+				while (1) {	
+					if (node->len < root->len) {
+						head = insertBeforeNode(&node, &root, head);
+						return head;
+					}
+					if (strcmp(node->word, root->word) == -1) {
+						head = insertBeforeNode(&node, &root, head);
+						return head;
+					}
+					else {
+						if (root->next == NULL) {
+							root->next = node;
+							return head;
+						}
+						root = root->next;
+					}
+				}
+			}
+			if (root->next == NULL) {
+				root->next = node;
+				return head;
+			}
+			root = root->next;
+		}
+	}
+}
+
+List_t* newNode() {
+	List_t* nod = (List_t*)malloc(sizeof(List_t));
+	if (!nod) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	nod->next = NULL;
+	return nod;
+}
+
+void printList(List_t* root, int N) {
+	while (root != NULL) {
+		if (root->len >= N) {
+			printf("%s ", root->word);
+		}
+		root = root->next;
+	}
+}
+
+void printList_oneLen(List_t* root, int N) {
+	while (root != NULL) {
+		if (root->len == N) {
+			printf("%s ", root->word);
+		}
+		root = root->next;
+	}
+}
+
+int main(void) {
+	char buffer[50];
+	setlocale(LC_ALL, "Rus");
+	List_t* root = (List_t*)malloc(sizeof(List_t));
+	if (!root) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	root->next = NULL;
+	List_t* newNod;
+	int s, N, n;
+	FILE* f;
+	if ((f = fopen("wordlist.txt", "rb")) == NULL)
+		printf("The file was not opened\n");
+	else {
+		s = fscanf(f, "%s", buffer);
+		addInfo(root, buffer);
+		s = fscanf(f, "%s", buffer);
+		while (s != EOF) {
+			newNod = newNode();
+			addInfo(newNod, buffer);
+			root = Sort_List(root, newNod);
+			s = fscanf(f, "%s", buffer);
+		}
+		printf("Введите минимальное число символов в слове: ");
+		scanf("%d", &N);
+		printList(root, N);
+		printf("\nСлова с какой длинной Вы хотите найти? ");
+		scanf("%d", &n);
+		printList_oneLen(root, n);
+	}
+	return 0;
+}
