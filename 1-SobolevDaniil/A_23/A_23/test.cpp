6de8716516928975f411fea257d1195dfd82616e
Parampaika <92088506+Parampaika@users.noreply.github.com>
2021-10-09 19:59:33 +0300

    Update test.cpp

--- 1-SobolevDaniil/A_23/A_23/test.cpp
+++ 1-SobolevDaniil/A_23/A_23/test.cpp
@@ -1,11 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "gtest/gtest.h"
-#include "A_23.c"
-
+extern "C" {
+#include "Header.h"
+}
 TEST(addInfo, adding_new_data_in_word) {
 	List_t* nod = (List_t*)malloc(sizeof(List_t));
 	if (!nod) {
@@ -21,7 +19,11 @@ TEST(addInfo, adding_new_data_in_word) {
 TEST(insertBeforeNode, checking_that_node_2_is_inserted_in_front_of_the_head) {
 	List_t* head_node_1 = (List_t*)malloc(sizeof(List_t));
 	List_t* node_2 = (List_t*)malloc(sizeof(List_t));
-	if (!head_node_1 || !node_2) {
+	if (!node_2) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!head_node_1) {
 		printf("Error allocating memory\n");
 		exit(1);
 	}
@@ -40,7 +42,15 @@ TEST(insertBeforeNode, checking_that_node_2_is_inserted_in_front_of_the_node_1_w
 	List_t* head = (List_t*)malloc(sizeof(List_t));
 	List_t* node_1 = (List_t*)malloc(sizeof(List_t));
 	List_t* node_2 = (List_t*)malloc(sizeof(List_t));
-	if (!head || !node_1 || !node_2) {
+	if (!node_1) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!head) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!node_2) {
 		printf("Error allocating memory\n");
 		exit(1);
 	}
@@ -54,14 +64,20 @@ TEST(insertBeforeNode, checking_that_node_2_is_inserted_in_front_of_the_node_1_w
 	head = insertBeforeNode(&node_2, &node_1, head);
 	ASSERT_STRCASEEQ(head->next->word, buf3);
 	ASSERT_STRCASEEQ(head->next->next->word, buf1);
+	free(head->next->next);
 	free(head->next);
 	free(head);
+
 }
 
 TEST(Sort_List, checking_that_the_shortest_word_goes_to_the_beginning) {
 	List_t* root = (List_t*)malloc(sizeof(List_t));
 	List_t* node = (List_t*)malloc(sizeof(List_t));
-	if (!root || !node) {
+	if (!node) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!root) {
 		printf("Error allocating memory\n");
 		exit(1);
 	}
@@ -80,7 +96,11 @@ TEST(Sort_List, check_for_sorting_by_alphabet_with_equal_length) {
 	List_t* root = (List_t*)malloc(sizeof(List_t));
 	List_t* node = (List_t*)malloc(sizeof(List_t));
 
-	if (!root || !node ) {
+	if (!node) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!root) {
 		printf("Error allocating memory\n");
 		exit(1);
 	}
@@ -100,7 +120,11 @@ TEST(Sort_List, check_for_sorting_by_length_node_more_then_root) {
 	List_t* root = (List_t*)malloc(sizeof(List_t));
 	List_t* node = (List_t*)malloc(sizeof(List_t));
 
-	if (!root || !node) {
+	if (!node) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!root) {
 		printf("Error allocating memory\n");
 		exit(1);
 	}
@@ -120,7 +144,15 @@ TEST(Sort_List, check_for_sorting_by_length_and_alphabet) {
 	List_t* root = (List_t*)malloc(sizeof(List_t));
 	List_t* node = (List_t*)malloc(sizeof(List_t));
 	List_t* node1 = (List_t*)malloc(sizeof(List_t));
-	if (!root || !node || !node1) {
+	if (!node) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!root) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	if (!node1) {
 		printf("Error allocating memory\n");
 		exit(1);
 	}
@@ -138,8 +170,11 @@ TEST(Sort_List, check_for_sorting_by_length_and_alphabet) {
 	ASSERT_STRCASEEQ(root->word, buf1);
 	ASSERT_STRCASEEQ(root->next->word, buf3);
 	ASSERT_STRCASEEQ(root->next->next->word, buf2);
+	
+	free(root->next->next); 
 	free(root->next);
 	free(root);
+
 }
 
 int main(int argc, char* argv[]) {
